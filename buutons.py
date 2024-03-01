b6425e2ae58b1151278f24e1693b8ca7e7703912
Evgeniy Gordeev <89735790+egordeev-ds@users.noreply.github.com>
2024-03-01 22:03:50 +0300

    обработка исключений

--- buutons.py
+++ buutons.py
@@ -78,12 +78,10 @@ def handle_strategy(message):
         #t0.2 - block0(подготовка)
         r.delete_all_orders()
         r.create_order_11()
-        order_price_11 = r.check_last_order_price()
         r.create_order_9()
-        order_price_9 = r.check_last_order_price()
         response = f"Закрыл все ордера - delete open orders.\n" \
-                   f"Выполнил order_11 прод. {order_price_11} - продал все btc.\n" \
-                   f"Выполнил order_9 пок. {order_price_9} - закупил резерв.\n" 
+                   f"Выполнил order_11 прод. - продал все btc.\n" \
+                   f"Выполнил order_9 пок. - закупил резерв.\n" 
         bot.send_message(message.chat.id, response)   
         time.sleep(sleep_time)
 
@@ -91,11 +89,10 @@ def handle_strategy(message):
         #t1.1 - block1 intro
         profit = round(r.calculate_total_balance() - enter_balance, 3)
         max_profit = round(profit, 3)
-        #УСЛОВИЕ
         lo_price = r.check_last_order_price()
         bot.send_message(message.chat.id, f"--Нахожусь в Блоке 1--\n"\
-                                          f"--LO_price={lo_price}--]n"\
-                                          f"--profit={profit}, max={max_profit}--")
+                                          f"--LO_price={lo_price}--\n"\
+                                          f"--profit={profit} USDC, max={max_profit} USDC--")
         time.sleep(sleep_time)
     
         #t1.2 - block1 action
@@ -104,14 +101,17 @@ def handle_strategy(message):
                 delta = int(r.check_market_price() - r.check_last_order_price())
                 if delta > form['ints']['int_0']:
                     metka = None
-                    r.create_order_enter_2()
-                    order_price = r.check_last_order_price()
-                    r.create_order_1()
-                    r.create_order_3() 
-                    r.create_order_4()                        
-                    bot.send_message(message.chat.id, f"delta={delta} ---> \n"\
-                                                    f"Выполнил enter2 пок {order_price}\n"\
-                                                    f"Выставил ордера 1-4")
+                    try:
+                        r.create_order_enter_2()
+                        order_price = r.check_last_order_price()
+                        r.create_order_1()
+                        r.create_order_3() 
+                        r.create_order_4()                   
+                        bot.send_message(message.chat.id, f"delta={delta} ---> \n"\
+                                                        f"Выполнил enter2 пок {order_price}\n"\
+                                                        f"Выставил ордера 1-4")
+                    except Exception:
+                        bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")     
                     break
                 elif delta < -form['ints']['int_9']:
                     metka = r.check_market_price()  
@@ -125,13 +125,16 @@ def handle_strategy(message):
             time.sleep(sleep_time)
         if message.text == "Стратегия 2(market)":
             metka = None
-            r.create_order_enter_1()
-            order_price = r.check_last_order_price()
-            r.create_order_1()
-            r.create_order_3()
-            r.create_order_4()
-            bot.send_message(message.chat.id, f"Выполнил enter1 пок. {order_price}\n"\
-                                            f"Выставил ордера 1-4")
+            try:
+                r.create_order_enter_1()
+                order_price = r.check_last_order_price()
+                r.create_order_1()
+                r.create_order_3()
+                r.create_order_4()
+                bot.send_message(message.chat.id, f"Выполнил enter1 пок. {order_price}\n"\
+                                                f"Выставил ордера 1-4")
+            except Exception:
+                bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")
             time.sleep(sleep_time)
         
 
@@ -162,8 +165,8 @@ def handle_strategy(message):
                 max_profit = profit if profit >= max_profit else max_profit                
                 lo_price = r.check_last_order_price()
                 bot.send_message(message.chat.id, f"--Нахожусь в Блоке 2--\n"\
-                                                f"--LO_price={lo_price}--]n"\
-                                                f"--profit={profit}, max={max_profit}--")
+                                                f"--LO_price={lo_price}--\n"\
+                                                f"--profit={profit} USDC, max={max_profit} USDC--")
                 time.sleep(sleep_time)
 
                 #t2.2 - block2 action
@@ -171,19 +174,25 @@ def handle_strategy(message):
                     delta = int(r.check_market_price() - r.check_last_order_price())
                     order_5_is_active = check_order5_active()
                     if delta < 0:
-                        metka = None
-                        r.delete_all_orders()
-                        r.create_order_10()       
-                        bot.send_message(message.chat.id, f"Закрыл все ордера - delete open orders.\n"\
-                                                          f"delta={delta} --->\n"\
-                                                          f"Выполнил order10 прод.") 
+                        try:
+                            metka = None
+                            r.delete_all_orders()
+                            r.create_order_10()       
+                            bot.send_message(message.chat.id, f"Закрыл все ордера - delete open orders.\n"\
+                                                            f"delta={delta} --->\n"\
+                                                            f"Выполнил order10 прод.") 
+                        except Exception:
+                            bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")
                         break
                     elif order_5_is_active == False: 
-                        metka = None
-                        # r.delete_all_orders() - александр попросил закомментить
-                        r.create_order_5()                         
-                        bot.send_message(message.chat.id, f"delta={delta} и order5={order_5_is_active} --->\n"\
-                                                          f"Выставил order5 прод.") 
+                        try:
+                            metka = None
+                            # r.delete_all_orders() - александр попросил закомментить
+                            r.create_order_5()                         
+                            bot.send_message(message.chat.id, f"delta={delta} и order5={order_5_is_active} --->\n"\
+                                                            f"Выставил order5 прод.") 
+                        except Exception:
+                            bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")
                         break
                     else:
                         metka = None
@@ -197,8 +206,8 @@ def handle_strategy(message):
                 max_profit = profit if profit >= max_profit else max_profit                
                 lo_price = r.check_last_order_price()
                 bot.send_message(message.chat.id, f"--Нахожусь в Блоке 3--\n"\
-                                                f"--LO_price={lo_price}--]n"\
-                                                f"--profit={profit}, max={max_profit}--")
+                                                f"--LO_price={lo_price}--\n"\
+                                                f"--profit={profit} USDC, max={max_profit} USDC--")
                 r.delete_all_orders()
                 bot.send_message(message.chat.id, "Закрыл все ордера - delete open orders")
                 time.sleep(sleep_time)
@@ -207,17 +216,19 @@ def handle_strategy(message):
                 while is_running:                    
                     delta = int(r.check_market_price() - r.check_last_order_price())
                     if delta > form['ints']['int_8']:
-                        metka = None
-                        r.delete_all_orders()
-                        r.create_order_8()
-                        order_price = r.check_last_order_price()
-                        r.create_order_1()
-                        r.create_order_3() 
-                        r.create_order_4()
-                        bot.send_message(message.chat.id, 
-                                         text = f"delta={delta} --->\n"\
-                                                f"Выполнил order8 пок. {order_price}\n"\
-                                                f"Выставил ордера 1-4") 
+                        try:
+                            metka = None
+                            r.create_order_8()
+                            order_price = r.check_last_order_price()
+                            r.create_order_1()
+                            r.create_order_3() 
+                            r.create_order_4()
+                            bot.send_message(message.chat.id, 
+                                            text = f"delta={delta} --->\n"\
+                                                    f"Выполнил order8 пок. {order_price}\n"\
+                                                    f"Выставил ордера 1-4") 
+                        except Exception:
+                            bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")
                         break
                     elif delta < -form['ints']['int_9']:
                         metka = r.check_market_price()
@@ -235,27 +246,29 @@ def handle_strategy(message):
                 profit = round(r.calculate_total_balance() - enter_balance, 3)
                 max_profit = profit if profit >= max_profit else max_profit                
                 bot.send_message(message.chat.id, f"--Нахожусь в Блоке 4--\n"\
-                                                f"--LO_price={metka}--]n"\
-                                                f"--profit={profit}, max={max_profit}--")
+                                                f"--LO_price={metka}--\n"\
+                                                f"--profit={profit} USDC, max={max_profit} USDC--")
                 time.sleep(sleep_time)
 
                 #t4.2 - block4 action
                 while is_running:
                     delta = int(r.check_market_price() - metka)
                     if delta > form['ints']['int_7']:
-                        metka = None
-                        r.delete_all_orders()
-                        r.create_order_7()
-                        order_price = r.check_last_order_price()
-                        r.create_order_1()
-                        r.create_order_3() 
-                        r.create_order_4()                        
-                        bot.send_message(message.chat.id,
-                                         text = f"Закрыл все ордера - delete open orders\n"\
-                                                f"delta={delta} --->\n"\
-                                                f"Выполнил order7 пок. {order_price}\n"\
-                                                f"Выставил ордера 1-4\n"\
-                                                f"Обнулил значение метки")                        
+                        try:
+                            metka = None
+                            r.delete_all_orders()
+                            r.create_order_7()
+                            order_price = r.check_last_order_price()
+                            r.create_order_1()
+                            r.create_order_3() 
+                            r.create_order_4()                        
+                            bot.send_message(message.chat.id, f"Закрыл все ордера - delete open orders\n"\
+                                                            f"delta={delta} --->\n"\
+                                                            f"Выполнил order7 пок. {order_price}\n"\
+                                                            f"Выставил ордера 1-4\n"\
+                                                            f"Обнулил значение метки")  
+                        except Exception:
+                            bot.send_message(message.chat.id, "Ошибка. Перезапустите бота")                                                  
                         break
                     elif delta < -form['ints']['int_10']:
                         metka = r.check_market_price()
